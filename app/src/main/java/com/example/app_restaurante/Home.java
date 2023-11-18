6569d2bcab028db33873b56a3934f643bf1b42ca
DanielCardona2002 <143425896+DanielCardona2002@users.noreply.github.com>
2023-11-18 17:10:24 -0500

    Vista de home creada

---
+++ app/src/main/java/com/example/app_restaurante/Home.java
@@ -0,0 +1,39 @@
+package com.example.app_restaurante;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.ComponentActivity;
+
+public class Home extends ComponentActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+    }
+
+    public void Gestion_Empleados(View view) {
+
+    }
+
+    public void Gestion_Inventario(View view){
+
+    }
+
+    public void Gestion_Mesas(View view){
+
+    }
+
+    public void Gestion_Menu(View view){
+
+    }
+
+    public void Gestion_Reservacion(View view){
+
+    }
+
+    public void Gestion_Pedidos(View view){
+
+    }
+}
