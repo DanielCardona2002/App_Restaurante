a90d621f42bd966a750f2407749cc2195cff6b99
DanielCardona2002 <143425896+DanielCardona2002@users.noreply.github.com>
2023-11-18 16:36:14 -0500

    Vista de Logeo creada

---
+++ app/src/main/java/com/example/app_restaurante/Login.java
@@ -0,0 +1,22 @@
+package com.example.app_restaurante;
+import android.os.Bundle;
+import android.view.View;
+import androidx.activity.ComponentActivity;
+
+public class Login extends ComponentActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+    }
+
+    public View Loggeo(){
+        return null;
+    }
+
+    public View Registro(){
+        return null;
+    }
+
+}
---
+++ app/src/main/java/com/example/app_restaurante/Register.java
@@ -0,0 +1,14 @@
+package com.example.app_restaurante;
+
+import android.os.Bundle;
+
+import androidx.activity.ComponentActivity;
+
+public class Register extends ComponentActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+    }
+}
